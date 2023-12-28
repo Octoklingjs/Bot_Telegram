c21bbd159db5b13a6d711053b937cc0153abd815
Octokling <logangaillard@outlook.fr>
2023-12-28 10:25:06 +0100

    updates

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ const api = new telegram({
     token: process.env.TOKEN
 })
 
-const mp = new telegram.GetUpdateMessageProvider() //Initialisation d'envoie de messages ?
+const mp = new telegram.GetUpdateMessageProvider() //Initialisation d'envoie et de réception de message ?
+
+api.setMessageProvider(mp);
+api.start()
+.then(() => {
+    console.log("Le bot est allumé !")
+})
+.catch(console.err)
+
+api.on("update", update =>{ //Lorsque le bot est sollicité
+    console.log(update);
+})
+
 
 // Les informations sur le bot
-api.getMe()
-.then(console.log)
-.catch(console.err)
+function getInfos(){
+    api.getMe()
+    .then(console.log) //Si des informations ont été reçu
+    .catch(console.err) //Si un message d'erreur à été envoyé
+}
