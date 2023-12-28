a30192cd2e6d2e93e478da17347c920938ebf14c
Octokling <logangaillard@outlook.fr>
2023-12-28 10:09:15 +0100

    ?

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var telegram = require("telegram-bot-api");
 var dotenv = require("dotenv");
 dotenv.config()
 
-console.log(process.env.TOKEN)
-
 const api = new telegram({
     token: process.env.TOKEN
 })
+
+const mp = new telegram.GetUpdateMessageProvider() //Initialisation d'envoie de messages ?
+
+// Les informations sur le bot
+api.getMe()
+.then(console.log)
+.catch(console.err)
