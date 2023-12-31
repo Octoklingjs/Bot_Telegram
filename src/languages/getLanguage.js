e3f051ce1b1090b1be4078a74eea1c927d061bf4
Octokling <logangaillard@outlook.fr>
2023-12-31 17:41:38 +0100

    getLanguage.js finished

--- src/languages/getLanguage.js
+++ src/languages/getLanguage.js
@@ -1,23 +1,33 @@
 //Importation de tout les json de langages
-const { promises } = require("dns")
 const listLang = require("./listLang.json")
-var langAvailable;
-
-
-Object.keys(listLang).forEach((key) => {
-    console.log(key)
-})
+const fs = require("fs")
+var langAvailable = listLang;
 
 /***
+ * Récuperer le contenu d'un message à envoyé pré enregistré en échange d'un code
  * @param lang La langue que nous voulons récupérer
- * @param type Le type de message recherché [0: Messages, 1: Erreurs, 2: COMMING SOON)]
+ * @param type Le type de message recherché [messages: Messages | errors: Erreurs]
+ * @param msgCode Code du message
+ * @author Octokling
  */
-
 module.exports.getMessageLanguage = async function (lang, type, msgCode){
-    return langAvailable;
-    /*return new Promise((resolve, reject) => {
-        if(lang && )
-    })*/
-}
+    return new Promise((resolve, reject) => {
+        if(lang && type && msgCode){
+            if(langAvailable.hasOwnProperty(lang)){
+                if(langAvailable[lang].hasOwnProperty(type)){
 
-this.getMessageLanguage("fr", 0)
+                    fs.readFile(langAvailable[lang][type], "utf8", (error, data) => {
+                        if (!error){
+                            data = JSON.parse(data)
+
+                            if(data.hasOwnProperty(msgCode)){
+                                resolve(data[msgCode])
+
+                            }else{reject("Le code du message recherché n'est pas disponible ou mal écrite")}
+                        }else{reject("Erreur FS : " + error);}
+                    })
+                }else{reject("Le type recherché n'est pas disponible ou mal écrite")}
+            }else{reject("La langue recherché n'est pas disponible ou mal écrite")}
+        }else{reject("Il manque des arguments !")}
+    })
+}
