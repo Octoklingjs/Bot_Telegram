d9db60f6f6352b0f6e64854a01f3a4967a50e34e
Octokling <logangaillard@outlook.fr>
2024-01-01 13:01:35 +0100

    Uninstall openai xDD

--- tests/test.js
+++
@@ -1,6 +0,0 @@
-var messages = require("../src/languages/getMessages.js")
-
-messages.getMessageLanguage("fr", "messages", "test").then(result =>{
-    console.log(result)
-})
- //Cela marche vraiment comme j'ai envie que ça marche 
